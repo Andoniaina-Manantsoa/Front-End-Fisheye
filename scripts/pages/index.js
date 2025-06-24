785eda1aa6b81d0b54b4e217bc4db7dc13696e92
Andoniaina <amrazanadrakoto@gmail.com>
2025-06-24 15:40:23 +0200

    add photographers in page d'acceuil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,61 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+    // Récuperer les Elements JSON
+    const reponse = await fetch('./data/photographers.json')
+    const photographersData = await reponse.json();
+    return (photographersData.photographers)
+}
+
+function photographerTemplate(photographer) {
+    const { name, portrait, city, country, tagline, price } = photographer;
+    const picture = `assets/photographers/id-photos/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', picture);
+
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        location.classList.add('photographer-location');
+
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.classList.add('photographer-tagline');
+
+        const tarif = document.createElement('p');
+        tarif.textContent = `${price} €/jour`;
+        tarif.classList.add('photographer-price');
+
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(tarif);
+
+        return article;
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    return { getUserCardDOM };
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
+
