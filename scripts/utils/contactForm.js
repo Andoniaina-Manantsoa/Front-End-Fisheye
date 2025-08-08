4ae98c11ffa0dbfff15c5ec1b5e5f105b518306c
Andoniaina <amrazanadrakoto@gmail.com>
2025-08-08 10:56:11 +0200

    contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,11 @@
+console.log("contactForm");
+const contactButton = document.querySelector(".contact_button");
+console.log(contactButton);
+contactButton.addEventListener("click", () => alert("contacter"));
 function displayModal() {
+    console.log("contactez-nous");
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
