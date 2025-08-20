8b02d2eb5a516be3e00cd55e0704e53014eac9bf
Andoniaina <amrazanadrakoto@gmail.com>
2025-08-20 15:17:28 +0200

    modif style

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -105,9 +105,9 @@ function mediaFactory(media, { index, onOpen } = {}) {
         });
 
         // Clic sur image ou vidéo → ouvre Lightbox
-        mediaElement.addEventListener("click", () => {
+        /*mediaElement.addEventListener("click", () => {
             if (typeof onOpen === "function") onOpen(index);
-        });
+        });*/
 
         const infoContainer = document.createElement("div");
         infoContainer.classList.add("media-info");
